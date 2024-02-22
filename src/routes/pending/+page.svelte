6fb5daf2258ccf1b450adfe4f18cfe09dc0fdbf9
itsmikesharescode <mikejohneviota@gmail.com>
2024-02-22 21:13:10 +0800

    adding application details

--- src/routes/pending/+page.svelte
+++ src/routes/pending/+page.svelte
@@ -8,6 +8,8 @@
 	import { toast } from "svelte-sonner";
 	import type { PageServerData } from "./$types";
 	import { navState } from "$lib";
+	import Button from "$lib/components/ui/button/button.svelte";
+	import ShowDetails from "./ShowDetails.svelte";
 
     export let data: PageServerData;
 
@@ -42,58 +44,59 @@
     
 </script>
 <div class="p-2 flex flex-col gap-2 sm:max-w-[80%] mx-auto" in:fade>
+    {#if data.appData}
+        <Pagination.Root count={data.appData.data?.length ?? 1} perPage={10} let:pages let:currentPage>
+            <Pagination.Content class="mt-5">
+                
+                {#each pages as page (page.key)}
+                    {#if page.type === "ellipsis"}
+                        <Pagination.Item>
+                            
+                        <Pagination.Ellipsis />
+                        </Pagination.Item>
+                    {:else}
+                        <Pagination.Item >
+                            <form method="post" action="?/paginate" enctype="multipart/form-data" use:enhance={paginatingNews}>
+                                <Pagination.Link {page} isActive={currentPage == page.value} type="submit" name="pageReq" value={page.value} >
+                                    {page.value}
+                                </Pagination.Link>
+                            </form>
+                        </Pagination.Item>
+                    {/if}
+                {/each}
+                
+            </Pagination.Content>
+
+        </Pagination.Root>
+    {/if}
     
     <Table.Root>
-        <Table.Caption>A list of pending applications.</Table.Caption>
         <Table.Header >
         <Table.Row>
             <Table.Head class="w-[200px] truncate">Application Type</Table.Head>
             <Table.Head class="w-full">Full Name</Table.Head>
             <Table.Head class="w-[200px]">Status</Table.Head>
-           
+            
         </Table.Row>
         </Table.Header>
 
         <Table.Body class="">
-           
+            
             {#each pendingArray ?? []  as pendingApp }
                 <Table.Row>
                     <Table.Cell class="font-medium">{pendingApp.application_type.toUpperCase()}</Table.Cell>
-                    <Table.Cell>{pendingApp.user_fullname}</Table.Cell>
+                    <Table.Cell>
+                        <ShowDetails {pendingApp} />
+                    </Table.Cell>
                     <Table.Cell class="truncate">{pendingApp.is_accepted ? "Accepted" : "Pending"}</Table.Cell>
-                  
+                    
                 </Table.Row>
             {/each}
 
         </Table.Body>
         
     </Table.Root>
-
-    
-    <Pagination.Root count={data.appData?.data?.length ?? 1} perPage={10} let:pages let:currentPage>
-        <Pagination.Content class="mt-5">
-            
-            {#each pages as page (page.key)}
-                {#if page.type === "ellipsis"}
-                    <Pagination.Item>
-                        
-                    <Pagination.Ellipsis />
-                    </Pagination.Item>
-                {:else}
-                    <Pagination.Item >
-                        <form method="post" action="?/paginate" enctype="multipart/form-data" use:enhance={paginatingNews}>
-                            <Pagination.Link {page} isActive={currentPage == page.value} type="submit" name="pageReq" value={page.value} >
-                                {page.value}
-                            </Pagination.Link>
-                        </form>
-                    </Pagination.Item>
-                {/if}
-            {/each}
-            
-        </Pagination.Content>
-
-    </Pagination.Root>
     
 
-   
+
 </div>
---
+++ src/routes/pending/ShowDetails.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import * as AlertDialog from "$lib/components/ui/alert-dialog";
+	import type { ApplicationModel, FormModel } from "$lib/types";
+    import Button from "$lib/components/ui/button/button.svelte";
+
+    export let pendingApp: ApplicationModel;
+
+    let appDetails: FormModel = JSON.parse(pendingApp.user_application);
+
+</script>
+
+
+
+<AlertDialog.Root>
+
+    <AlertDialog.Trigger>
+        <Button variant="link">{pendingApp.user_fullname}</Button>
+    </AlertDialog.Trigger>
+
+    <AlertDialog.Content class="sm:max-w-[700px]">
+        <AlertDialog.Header>
+            <AlertDialog.Title>{pendingApp.application_type.toUpperCase()}</AlertDialog.Title>
+
+            <AlertDialog.Description class="text-left flex flex-col gap-4">
+                <div class="">
+                    <p class="font-bold">Selected Course:</p>
+                    <p class="text-left">{appDetails.selected_course}</p>
+                </div>
+
+                <div class="">
+                    <p class="font-bold">Present Address:</p>
+                    <p class="text-left">{appDetails.present_address}</p>
+                </div>
+
+                <div class="">
+                    <p class="font-bold">Religion:</p>
+                    <p class="text-left">{appDetails.religion}</p>
+                </div>
+
+                <div class="">
+                    <p class="font-bold">Date Of Birth:</p>
+                    <p class="text-left">{appDetails.date_of_birth}</p>
+                </div>
+
+                <div class="">
+                    <p class="font-bold">Fathers Name:</p>
+                    <p class="text-left">{appDetails.fathers_fname}</p>
+                </div>
+
+                <div class="">
+                    <p class="font-bold">Mothers Name:</p>
+                    <p class="text-left">{appDetails.mothers_fname}</p>
+                </div>
+            </AlertDialog.Description>
+
+        </AlertDialog.Header>
+
+        <AlertDialog.Footer>
+            <AlertDialog.Cancel>Hide</AlertDialog.Cancel>
+            <AlertDialog.Action>
+                {pendingApp.is_accepted ? "Decline" : "Accept"}
+            </AlertDialog.Action>
+        </AlertDialog.Footer>
+    </AlertDialog.Content>
+</AlertDialog.Root>
